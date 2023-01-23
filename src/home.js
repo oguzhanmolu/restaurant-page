f4a66eb347a30b8c329698f53f4cd81faaea76df
Oguzhanmolu <oguzhanmolu@pop-os.localdomain>
2023-01-23 14:46:03 +0300

    home page done

--- src/home.js
+++ src/home.js
@@ -1,9 +1,24 @@
 function createHome() {
   const home = document.createElement('div');
-  const homeText = document.createElement('p');
-  homeText.textContent = 'Test123';
-  home.appendChild(homeText);
+  const homeLogo = document.createElement('img');
+  const japaneseText = document.createElement('p');
+  const englishText = document.createElement('p');
+
+  homeLogo.src = '/dist/img/home-logo.png';
+  homeLogo.alt = 'Welcome home image';
+  homeLogo.classList.add('home-logo');
+
+  home.appendChild(homeLogo);
+  home.appendChild(createParagraph('ようこそ、 京都の春風へ!'));
+  home.appendChild(createParagraph(`Welcome to Kyoto's Harukaze!`));
+  home.appendChild(createParagraph(`We are honored to serve you since 1908.`));
   return home;
+}
+
+function createParagraph(text) {
+  const para = document.createElement('p');
+  para.textContent = text;
+  return para;
 }
 function loadHome() {
   const main = document.getElementById('main');
