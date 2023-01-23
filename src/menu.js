71c6578686c98c64da43f6a8f26a418f7c45a19a
Oguzhanmolu <oguzhanmolu@pop-os.localdomain>
2023-01-23 19:30:23 +0300

    fixed a few names and added comments

--- src/menu.js
+++ src/menu.js
@@ -1,18 +1,22 @@
+// Create the whole menu
 function createMenu() {
   const menu = document.createElement('div');
   menu.classList.add('menu-main');
-  menu.appendChild(createMenuItem('sushi1', 'Fat Roll Set'));
-  menu.appendChild(createMenuItem('sushi2', 'Mix Sushi Set'));
-  menu.appendChild(createMenuItem('ramen1', 'Naruto Hokage Special'));
-  menu.appendChild(createMenuItem('udon', `Chef's special Udon`));
+
+  menu.appendChild(createMenuItem('sushi1', 'Futomaki'));
+  menu.appendChild(createMenuItem('sushi2', 'Mix Sushi Rolls'));
+  menu.appendChild(createMenuItem('ramen1', 'Naruto Special Ramen'));
+  menu.appendChild(createMenuItem('udon', `Chef's Legendary Udon`));
   menu.appendChild(createMenuItem('bbqset', 'BBQ Lunch Set'));
   menu.appendChild(createMenuItem('omurice', 'Omurice'));
   menu.appendChild(createMenuItem('onigiri', 'Tuna Mayo Onigiri'));
   menu.appendChild(createMenuItem('gyoza', 'Gyoza'));
   menu.appendChild(createMenuItem('yakitori', 'Yakitori'));
+
   return menu;
 }
 
+// Create menu items
 function createMenuItem(itemName, itemDescription) {
   const menuItemGroup = document.createElement('div');
   const menuItemImg = document.createElement('img');
@@ -30,10 +34,12 @@ function createMenuItem(itemName, itemDescription) {
   return menuItemGroup;
 }
 
+// Load menu on main
 function loadMenu() {
   const main = document.getElementById('main');
   main.textContent = '';
   main.appendChild(createMenu());
   return main;
 }
+
 export default loadMenu;
